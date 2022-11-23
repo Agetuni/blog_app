a55da5f7d3f0436df6d441a0171465eda3d626b7
Aleazar yilma <yilma4060@gmail.com>
2022-11-23 21:13:10 +0300

    creat post model

--- db/migrate/20221123175956_create_user.rb
+++ db/migrate/20221123175956_create_user.rb
@@ -1,10 +1,10 @@
 class CreateUser < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :photo
+      t.string :Name
+      t.string :Bio
       t.text :bio
-      t.integer :post_counter
+      t.integer :Post_counter
       t.timestamps
     end
   end

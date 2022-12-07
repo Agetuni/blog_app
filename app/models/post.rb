0840cbb6449d6a63fcdc6d18e8ccdda7fb93948b
Aleazar yilma <yilma4060@gmail.com>
2022-12-07 20:29:20 +0300

    add authorization

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', counter_cache: :posts_counter
-  has_many :comments
+  has_many :comments, dependent: :delete_all
   has_many :likes
   validates :title, presence: true, length: { maximum: 250 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
@@ -8,5 +8,9 @@ class Post < ApplicationRecord
 
   def last_five_comments
     comments.last(5)
+  end
+  
+  def update_posts_counter
+    author.increment!(:posts_counter)
   end
 end
